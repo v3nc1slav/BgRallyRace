d43c14774d331a4c2257df8f4d57c60ed6620102
v3nc1slav <v.verchov@mail.bg>
2020-01-28 23:52:30 +0200

    add Services and Method GetOpinions and AddOpinions

---
+++ BgRallyRace/BgRallyRace/Services/OpinionsServices.cs
@@ -0,0 +1,35 @@
+﻿using BgRallyRace.Data;
+using BgRallyRace.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BgRallyRace.Services
+{
+    public class OpinionsServices
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public OpinionsServices()
+        {
+            
+        }
+        public OpinionsServices(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public void  AddOpinion(string text, IdentityUser user)
+        {
+            dbContext.Opinions.Add(new Opinions { Content= text, DateOfPublication = DateTime.UtcNow, User =  user });
+            dbContext.SaveChanges();
+        }
+
+        public string[] GetOpinions()
+        {
+            var result = dbContext.Opinions.Select(x=>x.Content).ToArray();
+            return result;
+        }
+    }
+}
